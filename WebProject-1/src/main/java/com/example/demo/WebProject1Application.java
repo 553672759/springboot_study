4a67df5b5ec6f20f42f949f3307ec722b20fa3b9
阿修 <553672759@qq.com>
2018-08-11 20:37:24 +0800

    startDemo

---
+++ WebProject-1/src/main/java/com/example/demo/WebProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class WebProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProject1Application.class, args);
+	}
+}
---
+++ WebProject-1/src/main/java/xx/controller/WebController.java
@@ -0,0 +1,32 @@
+package xx.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value=("/web"))
+public class WebController {
+	
+	@Value(value="${xx.secret}")
+	private String uuid;
+	
+	@Value(value="${xx.number}")
+	private int randomID;
+	
+	@Value(value="${xx.limitnumber}")
+	private int limitnumber;
+	
+	@RequestMapping(value="/index")
+	public Map<String,Object> Index(){
+		Map<String, Object> map = new HashMap<String,Object>();
+		map.put("uuid", uuid);
+		map.put("randomID", randomID);
+		map.put("limitnumber", limitnumber);
+		return map;
+		
+	}
+}
